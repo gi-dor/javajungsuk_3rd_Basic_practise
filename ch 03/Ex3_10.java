c8b97892779e0345eccf0b747d7f73ca000687c8
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-01-29 18:13:18 +0900

    Add files via upload

---
+++ ch 03/Ex3_10.java
@@ -0,0 +1,28 @@
+package _ch03;
+
+
+public class Ex3_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		
+	
+		   // 문자열 비교 
+		
+				String str1 = "abc";
+				String str2 = "abc";
+				System.out.println(str1 == str2);   // true
+				System.out.println(str1.equals(str2));   // true
+
+				String str3 = new String("abc");
+				String str4 = new String("abc");
+				System.out.println(str3==str4);  //false
+				System.out.println(str3.equals(str4));   //true
+		
+		
+		
+		
+	}
+
+}
