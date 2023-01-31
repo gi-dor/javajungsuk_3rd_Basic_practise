a53e7fb97d6207192d2b1d40f73ed11bb7a0dfd8
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-01-31 20:12:50 +0900

    x,y,z 값을 서로 바꾸기

---
+++ day230131_04.java
@@ -0,0 +1,36 @@
+package day02;
+import java.util.Scanner;
+public class day230131_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("1"+"2");
+		System.out.println(""+true);
+		System.out.println('A'+'B');
+		System.out.println('1'+2);
+		System.out.println('1'+'2');
+		System.out.println('j'+"ava");
+	//	System.out.println(true+null);
+		
+		
+	
+		System.out.println("---------------------");
+		
+		int x = 1;
+		int y =2;
+		int z =3;
+		
+		int tmp = 0; 
+		
+		tmp = x ;
+		x = y;
+		y = z;
+		z = tmp;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+		
+	}
+
+}
