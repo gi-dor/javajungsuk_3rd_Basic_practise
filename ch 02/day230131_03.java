45755332a67338c206ea652c0835e67ea97a13d9
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-01-31 20:12:10 +0900

    Scanner

---
+++ ch 02/day230131_03.java
@@ -0,0 +1,19 @@
+package day02;
+import java.util.Scanner;
+public class day230131_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner scanner =new Scanner(System.in);
+			
+			System.out.println("1번 정수 입력.>>");
+			int num = scanner.nextInt();
+			
+			System.out.println("2번 실수입력.>>");
+			float num2 = scanner.nextFloat();
+			
+			System.out.println("입력한 값 : " +num );
+			System.out.println("입력한 값2 : "+num2);
+	}
+
+}
