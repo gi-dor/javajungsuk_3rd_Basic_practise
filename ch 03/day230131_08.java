e44e233f702d696e509354e3f7797ddaef3c4a3c
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-01-31 20:07:47 +0900

    소수점 첫째자리 반올림  Math.round()

---
+++ ch 03/day230131_08.java
@@ -0,0 +1,38 @@
+package day02;
+
+public class day230131_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double pi = 3.141593;
+		System.out.println("pi ="+pi);
+		
+		double pi0 = Math.round(pi*10)/10.0;
+		System.out.println("pi0 = "+pi0);
+		
+		double pi1 = Math.round(pi*100)/100.0;
+		System.out.println("pi1 = "+pi1);
+		
+		double pi2 = Math.round(pi*1000)/1000.0;
+		System.out.println("pi2 = "+pi2);
+		
+		double pi3 = Math.round(pi*10000)/10000.0;
+		System.out.println("pi3 = "+pi3);
+		
+		System.out.println("소수점 둘쨰자리 반올림 = "+ Math.round(pi*10)/10.0);
+		System.out.println("소수점 셋째자리 반올림 = "+Math.round(pi*100)/100.0);
+		System.out.println("소수점 넷째자리 반올림 = "+Math.round(pi*1000)/1000.0);
+		System.out.println("소수점 다섯째자리 반올림 = "+Math.round(pi*10000)/10000.0);
+			
+		// 1000.0 이 아닌 1000으로 나누기
+		// pi = 3.141593
+		
+				
+		
+		
+		
+	
+	}
+
+}
