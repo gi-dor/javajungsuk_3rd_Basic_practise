f7ed6949719b14071ff838251f0731bb0d3ba299
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-02-03 17:36:39 +0900

    Add files via upload

---
+++ ch 04/day230203_03.java
@@ -0,0 +1,37 @@
+package day05;
+
+public class day230203_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				// 구구단
+				for ( int i =2 ; i <=9; i++) {
+					for (int j =1; j <=9; j++) {
+						System.out.printf("%d * %d = %d%n", i , j ,i*j);
+					}
+					System.out.println();
+				}
+				System.out.println("1번");
+				System.out.println("**********");
+				System.out.println("**********");
+				System.out.println("**********");
+				System.out.println("**********");
+				System.out.println("**********");
+					System.out.println();
+					
+					System.out.println("2번");
+					// i= 1,2,3,4,5 {}안의 내용반복
+				for (int i =1; i<=5; i++) {
+					for (int j =1; j<=10; j++) {	
+						System.out.print("*");
+					} // 10개 찍고
+					System.out.println(); //줄바꿈			
+				}
+				
+
+				
+			
+	
+	}
+
+}
