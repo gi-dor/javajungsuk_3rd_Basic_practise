744297d3628f3d0beb377b9dec00641e96334e70
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-02-22 18:00:17 +0900

    다형성 - 참조변수 형변환 ,

---
+++ ch 07/day230222_02.java
@@ -0,0 +1,45 @@
+package day17;
+// 참조변수의 형변환 
+// 자손타입의 참조변수를 조상타입의 참조변수로
+// 조상타입의 참조변수를 자손타입의 참조변수로 형변환만 가능하다
+
+
+public class day230222_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = null;
+		FireEngine fe = new FireEngine(); // FireEngine 객체생성
+		FireEngine fe2 = null; 
+		
+		fe.water();
+		car = fe;						// fe 값을 car에 저장 
+		// car = (Car)fe;
+		fe2 = (FireEngine)car;
+		// 여기좀 헷갈림 
+		// 복습할때 확인하고 이해한 부분 적어두기 메모...
+		fe2.water();
+		
+	}
+
+}
+
+class Car {
+	String color;
+	int door;
+	
+	void drive() {
+		System.out.println("지나갑니다~");
+	}
+	
+	
+	void stop () {
+		System.out.println("나오십쇼 ~");
+	}
+}
+
+	class FireEngine extends Car{
+		void water() {
+			System.out.println("물 뿌립니다~");
+		}
+	}
