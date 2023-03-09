6665384a3ecb8c57ace39c7700d5c3e25225d811
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-03-09 20:51:42 +0900

    2차원 배열의 관해

---
+++ ch 05/day230309_11.java
@@ -0,0 +1,40 @@
+package day27;
+
+public class day230309_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// 2차원 배열
+			// 테이블 형태의 데이터를 저장하기 위한 배열
+			// int[] [] score = new int [4][3];	// 4행 3열의 2차원 배열 생성 
+			// 총 12개의 값을 저장할수 있음
+		
+			// 2차원 배열의 선언
+			// 타입[][] 변수이름 ;  int[][] score;
+			// 타입 변수이름 [][];  int score [][];
+			// 타입[] 변수이름[];   int[] score[];
+		
+			// 2차원 배열의 인덱스
+			// int [][] score = new int[4][3];
+			// 행 인덱스 : 0 ~ 4-1  0,1,2,3
+			// 열 인덱스 : 0 ~ 3-1  0,1,2
+			// score[0][0]  score[0][1]  score[0][2]
+			// score[1][0]  score[1][1]  score[1][2]
+			// score[2][0]  score[2][1]  score[2][2]
+			// score[3][0]  score[3][1]  score[3][2]
+		
+			// score[0][0] =100; 배열 score의 1행 1열에 100을 저장 
+			// System.out.println(score[0][0]);	배열 score의 1행 1열의 값을 출력
+		
+			// 2차원 배열의 초기화 
+			// int [][]score = { {1,2,3} , {4,5,6} };
+			// int[][] score = {
+			//						{1,2,3},
+			//						{4,5,6}
+			//					};
+		
+		
+		
+	}
+
+}
