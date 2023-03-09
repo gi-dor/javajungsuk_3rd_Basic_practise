9448cab9ae59928a5e5e1a472522a7e0b8c66621
gi-dor <86302876+gi-dor@users.noreply.github.com>
2023-03-09 20:52:15 +0900

    ch5 연습문제 5-4   2차원 배열의 총합과 평균 구하기

---
+++ ch 05/3판 예제문제/day230309_12.java
@@ -0,0 +1,32 @@
+package day27;
+
+public class day230309_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			int[][] arr= {
+					{ 5,5,5,5,5,},
+					{10,10,10,10,10},
+					{20,20,20,20,20},
+					{30,30,30,30,30}
+			};
+			
+			int total = 0;
+			float average = 0;
+			
+			for( int i = 0; i< arr.length;i++) {
+				for(int j = 0; j < arr[i].length; j++) {
+					System.out.printf("arr[%d][%d] = %d%n",i,j,arr[i][j]);
+					
+					total = total + arr[i][j];
+					average = (float)total/ (arr[i].length*arr.length);  // arr[i].length = 5; , arr.length = 4
+				}		// 325.0 / 20
+			}
+			
+			System.out.println("total = "+total);
+			System.out.println("average = "+average);
+	}
+
+}
